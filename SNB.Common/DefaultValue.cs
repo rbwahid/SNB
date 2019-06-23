f9a219664dadf611c97ae63a3464b7ffb05b3e28
Mahmud Koli <mahmudkolirpi@gmail.com>
2019-06-24 01:03:14 +0600

    Theme Implement...

---
+++ SNB.Common/DefaultValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNB.Common
+{
+    public static class DefaultValue 
+    {
+        public static string CompanyFullName => "Shopno Nibash";
+        public static string CompanyShortName => "SNB";
+        public static string DeveloperCompanyFullName => "Encoders Infotech Limited";
+        public static string DeveloperCompanyShortName => "Encoders";
+    }
+}
